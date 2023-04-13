932255b869a33f1e32496bc118f84cfc776ffb11
Brian rego <brian.rego03@gmail.com>
2023-04-13 16:42:25 +0530

    Add CSS and JS for 16*16 grid

--- scripting.js
+++ scripting.js
@@ -1,7 +1,22 @@
-const divA=document.createElement("div");
-divA.style.border="5px black";
-for(let i=0;i<(16*16);i++)
-    {
-        document.getElementsByClassName("SketchBoard").appendChild(divA);
+const divB= document.querySelector(".SketchBoard");  
+let divA;
+function sketch(){
+             
+    
+    for(let i=0;i<(16*16);i++)
+    {   
+        divA=document.createElement('div')
+        divA.style.border="1px solid red";
+        divA.style.width="65px";
+        divA.style.height="65px";
+        divA.style.margin="0px";
+        divB.appendChild(divA);
+        
+        console.log(divB);
     }
+    
+    
 
+}
+
+sketch();
