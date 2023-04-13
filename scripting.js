80422953ffee734468873332a193416a76aa0e3b
Brian rego <brian.rego03@gmail.com>
2023-04-13 14:44:04 +0530

    Create basic JS syntax for 16*16 sketchboard

---
+++ scripting.js
@@ -0,0 +1,7 @@
+const divA=document.createElement("div");
+divA.style.border="5px black";
+for(let i=0;i<(16*16);i++)
+    {
+        document.getElementsByClassName("SketchBoard").appendChild(divA);
+    }
+
