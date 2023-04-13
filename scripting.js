4adb621c8b1f5ff1c88d3c2f9df429ee438a3bc4
Brian rego <brian.rego03@gmail.com>
2023-04-13 16:57:35 +0530

    Separate append function and add class to children

--- scripting.js
+++ scripting.js
@@ -5,18 +5,20 @@ function sketch(){
     
     for(let i=0;i<(16*16);i++)
     {   
-        divA=document.createElement('div')
+        divA=document.createElement('div');
         divA.style.border="1px solid red";
         divA.style.width="65px";
         divA.style.height="65px";
         divA.style.margin="0px";
-        divB.appendChild(divA);
+        divB.appendChild(divA).classList.add("HoverAction");
         
-        console.log(divB);
+        
     }
     
     
 
 }
 
+
 sketch();
+
